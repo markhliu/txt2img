b743428a98e18fcd7e75bced5bec7f16f4695778
Mark Liu <50116107+markhliu@users.noreply.github.com>
2024-09-08 12:27:13 -0400

    Add files via upload

---
+++ utils/ViTutil.py
@@ -0,0 +1,182 @@
+import math
+import torch
+from torch import nn
+
+
+class GELU(nn.Module):
+    def forward(self, input):
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) *\
+                           (input + 0.044715 * torch.pow(input, 3.0))))
+
+class PatchEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.projection = nn.Conv2d(config.num_channels,
+                                    config.hidden_size, 
+                                    kernel_size=config.patch_size, 
+                                    stride=config.patch_size)
+    def forward(self, x):
+        x = self.projection(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x    
+    
+class Embeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.patch_embeddings = PatchEmbeddings(config)    
+        self.cls_token = nn.Parameter(torch.randn(1, 1, 
+                                      config.hidden_size))  
+        num_patches = (config.image_size // config.patch_size) ** 2
+        self.position_embeddings = \
+            nn.Parameter(torch.randn(1, num_patches + 1, config.hidden_size))  
+    def forward(self, x):
+        x = self.patch_embeddings(x)
+        batch_size, _, _ = x.size()
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.position_embeddings
+        return x    
+    
+    
+class AttentionHead(nn.Module):
+    def __init__(self, hidden_size, attention_head_size, bias=True):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.attention_head_size = attention_head_size
+        self.query = nn.Linear(hidden_size, attention_head_size, bias=bias)
+        self.key = nn.Linear(hidden_size, attention_head_size, bias=bias)
+        self.value = nn.Linear(hidden_size, attention_head_size, bias=bias)   
+    def forward(self, x):
+        query = self.query(x)
+        key = self.key(x)
+        value = self.value(x)
+        attention_scores = torch.matmul(query, key.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = nn.functional.softmax(attention_scores, dim=-1)
+        attention_output = torch.matmul(attention_probs, value)
+        return (attention_output, attention_probs)    
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = self.hidden_size // self.num_attention_heads
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+        self.heads = nn.ModuleList([])
+        for _ in range(self.num_attention_heads):
+            head = AttentionHead(
+                self.hidden_size,
+                self.attention_head_size)
+            self.heads.append(head)
+        self.output_projection = nn.Linear(self.all_head_size, self.hidden_size) 
+    def forward(self, x, output_attentions=False):
+        attention_outputs = [head(x) for head in self.heads]
+        attention_output = torch.cat([attention_output for attention_output,
+                                      _ in attention_outputs], dim=-1)
+        attention_output = self.output_projection(attention_output)
+        if not output_attentions:
+            return (attention_output, None)
+        else:
+            attention_probs = torch.stack([attention_probs for _, 
+                           attention_probs in attention_outputs], dim=1)
+            return (attention_output, attention_probs)    
+    
+    
+
+
+
+class MLP(nn.Module):
+    """
+    A multi-layer perceptron module.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense_1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.activation = GELU()
+        self.dense_2 = nn.Linear(config.intermediate_size, config.hidden_size)
+
+    def forward(self, x):
+        x = self.dense_1(x)
+        x = self.activation(x)
+        x = self.dense_2(x)
+        return x    
+    
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.attention = MultiHeadAttention(config)
+        self.layernorm_1 = nn.LayerNorm(config.hidden_size)
+        self.mlp = MLP(config)
+        self.layernorm_2 = nn.LayerNorm(config.hidden_size)
+    def forward(self, x, output_attentions=False):
+        attention_output, attention_probs = \
+        self.attention(self.layernorm_1(x), output_attentions=output_attentions)
+        x = x + attention_output
+        mlp_output = self.mlp(self.layernorm_2(x))
+        x = x + mlp_output  
+        if not output_attentions:
+            return (x, None)
+        else:
+            return (x, attention_probs)
+
+class Encoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.blocks = nn.ModuleList([])
+        for _ in range(config.num_hidden_layers):
+            block = Block(config)
+            self.blocks.append(block)
+    def forward(self, x, output_attentions=False):
+        all_attentions = []
+        for block in self.blocks:
+            x, attention_probs = block(x, output_attentions=output_attentions)
+            if output_attentions:
+                all_attentions.append(attention_probs)
+        if not output_attentions:
+            return (x, None)
+        else:
+            return (x, all_attentions)
+
+class ViTForClassfication(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.image_size = config.image_size
+        self.hidden_size = config.hidden_size
+        self.num_classes = config.num_classes
+        self.embedding = Embeddings(config)
+        self.encoder = Encoder(config)
+        self.classifier = nn.Linear(self.hidden_size, self.num_classes)
+        self.apply(self._init_weights)
+    def forward(self, x, output_attentions=False):
+        embedding_output = self.embedding(x)
+        encoder_output, all_attentions = self.encoder(embedding_output,
+                      output_attentions=output_attentions)
+        logits = self.classifier(encoder_output[:, 0, :])
+        if not output_attentions:
+            return (logits, None)
+        else:
+            return (logits, all_attentions)
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Conv2d)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        elif isinstance(module, Embeddings):
+            module.position_embeddings.data = nn.init.trunc_normal_(
+                module.position_embeddings.data.to(torch.float32),
+                mean=0.0,std=0.02,).to(module.position_embeddings.dtype)
+            module.cls_token.data = nn.init.trunc_normal_(
+                module.cls_token.data.to(torch.float32),
+                mean=0.0,std=0.02,).to(module.cls_token.dtype)
+
+
+
+
+
